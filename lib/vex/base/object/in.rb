8426bb71f748eeb39d57290ade02b7e603ba8250
eno <eno@open-lab.org>
2010-05-23 14:26:20 +0200

    Adds 'in?' tests

--- lib/vex/base/object/in.rb
+++ lib/vex/base/object/in.rb
@@ -6,8 +6,20 @@
 # Model.find(1).insp 
 #    -> "<Model#1>"
 
-class Object
+module Object::InMethod
   def in?(collection)
     collection.include?(self)
+  end
+end
+
+class Object
+  include InMethod
+end
+
+module Object::InMethod::Etest
+  def test_in
+    assert_equal true, "1".in?(%w(1 2 3))
+    assert_equal false, 1.in?(%w(1 2 3))
+    assert_equal false, 1.in?([])
   end
 end
