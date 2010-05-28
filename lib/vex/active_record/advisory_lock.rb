e02b1bc8c8c8060b7564560a72052cbd5854f4a8
eno <eno@open-lab.org>
2010-05-28 21:53:53 +0200

    Fixed loading of files

--- lib/vex/active_record/advisory_lock.rb
+++ lib/vex/active_record/advisory_lock.rb
@@ -4,8 +4,12 @@ module ActiveRecord::AdvisoryLock
   TIMEOUT=10
 end
 
+#
+# The concrete implementations are in the advisory_lock directory.
+# They will be loaded by the init script, so we don't load them
+# here again. 
 # --- load advisory lock extensions -----------------------------------
 
-load "#{File.dirname(__FILE__)}/advisory_lock/mysql_adapter.rb"
+# load "#{File.dirname(__FILE__)}/advisory_lock/mysql_adapter.rb"
 # load "#{File.dirname(__FILE__)}/advisory_lock/postgresql_adapter.rb"
-load "#{File.dirname(__FILE__)}/advisory_lock/sqlite_adapter.rb"
+# load "#{File.dirname(__FILE__)}/advisory_lock/sqlite_adapter.rb"
