3bfaf2e70ebed4db944f9166bf8a015c288d152e
eno <eno@open-lab.org>
2010-08-27 14:16:58 +0200

    CGI.url_for accepts more than a single string parameter

--- lib/vex/base/cgi_ext.rb
+++ lib/vex/base/cgi_ext.rb
@@ -1,7 +1,16 @@
 require "cgi"
 
 class CGI
-  def self.build_url(url_base, opts = {})
+  def self.url_for(*args)
+    opts = case args.last
+    when Array, Hash then args.pop
+    else                  {}
+    end
+    
+    url_base = args.map do |arg|
+      arg.gsub(/(^\/)|(\/$)/, "")
+    end.join("/")
+    
     return url_base if opts.empty?
 
     url_base += url_base.index("?") ? "&" : "?"
@@ -15,13 +24,21 @@ class CGI
 end
 
 module CGI::Etest
-  def test_build_url
-    assert_equal "http://ix.de", CGI.build_url("http://ix.de")
-    assert_equal "http://ix.de?a", CGI.build_url("http://ix.de", :a => nil)
-    assert_equal "http://ix.de?a", CGI.build_url("http://ix.de", [ :a ])
-    assert_equal "http://ix.de?a=1", CGI.build_url("http://ix.de", :a => 1 )
-    assert_equal "http://ix.de?a=1", CGI.build_url("http://ix.de", [ [ :a, 1 ]] )
-    assert_equal "http://ix.de?a=1&b=bb", CGI.build_url("http://ix.de", [ [ :a, 1 ], [ :b, :bb ]] )
-    assert_equal "http://ix.de?a=b%3D1", CGI.build_url("http://ix.de", :a => "b=1" )
+  def test_url_for
+    assert_equal "http://ix.de", CGI.url_for("http://ix.de")
+    assert_equal "http://ix.de?a", CGI.url_for("http://ix.de", :a => nil)
+    assert_equal "http://ix.de?a", CGI.url_for("http://ix.de", [ :a ])
+    assert_equal "http://ix.de?a=1", CGI.url_for("http://ix.de", :a => 1 )
+    assert_equal "http://ix.de?a=1", CGI.url_for("http://ix.de", [ [ :a, 1 ]] )
+    assert_equal "http://ix.de?a=1&b=bb", CGI.url_for("http://ix.de", [ [ :a, 1 ], [ :b, :bb ]] )
+    assert_equal "http://ix.de?a=b%3D1", CGI.url_for("http://ix.de", :a => "b=1" )
+  end
+
+  def test_url_for_merging
+    assert_equal "http://ix.de/a/b", CGI.url_for("http://ix.de", "a", "b")
+    assert_equal "http://ix.de/a/b", CGI.url_for("http://ix.de/", "a/", "b")
+    assert_equal "http://ix.de/a/b", CGI.url_for("http://ix.de/", "/a/", "/b/")
+    assert_equal "http://ix.de//x/y", CGI.url_for("http://ix.de//", "/x/y")
+    assert_equal "http://ix.de//x///y", CGI.url_for("http://ix.de//", "/x///y")
   end
 end if VEX_TEST == "base"
